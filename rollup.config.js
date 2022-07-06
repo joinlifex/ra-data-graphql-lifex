73e9dcc3b86020ffb00bbea3453e1a53e7372105
Nathan <nathan@joinlifex.com>
2022-07-06 17:32:33 +0200

    feat: setup with rollup.

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,33 @@
-import copy from 'rollup-plugin-copy'
+import dts from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
 
-import { writeFile, mkdir } from 'fs/promises'
-
-function createCommonJsPackage() {
-  const pkg = { type: 'commonjs' }
-  return {
-    name: 'cjs-package',
-    buildEnd: async () => {
-      await mkdir('./dist', { recursive: true })
-      await writeFile('./dist/package.json', JSON.stringify(pkg, null, 2))
-    }
-  }
-}
+const bundle = config => ({
+  ...config,
+  input: 'src/index.ts',
+  external: id => !/^[./]/.test(id),
+})
 
 export default [
-  {
-    input: './src/index.js',
-    plugins: [
-      copy({
-        targets: [
-          { src: './package.json', dest: 'dist' }
-        ]
-      }),
-      createCommonJsPackage()
-    ],
+  bundle({
+    plugins: [esbuild()],
     output: [
-      { format: 'es', file: './dist/index.mjs' },
-      { format: 'cjs', file: './dist/index.cjs' }
-    ]
-  }
+      {
+        file: `dist/index.js`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: `dist/index.mjs`,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+  }),
+  bundle({
+    plugins: [dts()],
+    output: {
+      file: `dist/index.d.ts`,
+      format: 'es',
+    },
+  }),
 ]
